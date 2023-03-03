62e3f818cb57869bbaaba64c1e380198077062ba
Utku Ergün <111204328+utku0640@users.noreply.github.com>
2023-03-03 14:53:14 +0300

    Hacker News
    Hacker News

---
+++ src/context.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useReducer } from "react";
+
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+import reducer from "./reducer";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    dispatch({
+      type: "SET_STORIES",
+      payload: { hits: data.hits, nbPages: data.nbPages },
+    });
+  };
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, [state.query, state.page]);
+
+  const removeStory = (id) => {
+    dispatch({ type: "REMOVE_STORY", payload: id });
+  };
+  const handleSearch = (query) => {
+    dispatch({ type: "HANDLE_SEARCH", payload: query });
+  };
+  const handlePage = (value) => {
+    dispatch({ type: "HANDLE_PAGE", payload: value });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
